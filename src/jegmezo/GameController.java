b88b0088f1f481bd65bd1d75b678ed600d9d5b2b
Tamás <szoketamas98@gmail.com>
2020-03-28 10:50:30 +0100

    IceSheet-controller

--- src/jegmezo/GameController.java
+++ src/jegmezo/GameController.java
@@ -5,23 +5,30 @@ package jegmezo;
 
 /** */
 public class GameController {
+	private Tile[] tiles;
+	private Player[] players;
 	/** */
 	public void startGame() {
+		System.out.println("\nGameController startGame\n");
 	}
 	
 	/** */
 	public void gameOver() {
+		System.out.println("\nGameController gameOver\n");
 	}
 	
 	/** */
 	public void win() {
+		System.out.println("\nGameController win\n");
 	}
 	
 	/** */
 	public void blizzard() {
+		System.out.println("\nGameController blizzard\n");
 	}
 	
 	/** */
 	public void turn() {
+		System.out.println("\nGameController turn\n");
 	}
 }

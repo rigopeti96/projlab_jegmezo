cef4d1774e513b7acfddaee5c5166fae827fe626
Tamás <szoketamas98@gmail.com>
2020-03-28 10:57:05 +0100

    Hole

--- src/jegmezo/Hole.java
+++ src/jegmezo/Hole.java
@@ -6,25 +6,36 @@ package jegmezo;
 public class Hole extends Tile {
 	/** */
 	public void stepOnto(Player player, Tile prevTile) {
+		System.out.println("\nHole stepOnto\n");
 	}
 	
 	/** */
 	public int getPlayerLimit() {
+		System.out.println("\nHole getPlayerLimit\n");
+		return 0;
 	}
-	
+
 	/** */
 	public Item getItem() {
+		System.out.println("\nHole getItem\n");
+		return null;
 	}
 	
 	/** */
 	public boolean buildIgloo() {
+		System.out.println("\nHole buildIgloo\n");
+		return false;
 	}
-	
+
 	/** */
 	public void removeItem() {
+		System.out.println("\nHole removeItem\n");
 	}
 	
 	/** */
 	public boolean canSave() {
+		System.out.println("\nHole canSave\n");
+		return false;
 	}
+	
 }

e28e66160d5faad6ec144ebcab7176bf0dd0e22b
Péter Attila Rigó <rigopeter96@gmail.com>
2020-04-25 11:29:54 +0200

    Random blizzard megvalósítás

--- game/src/jegmezo/GameController.java
+++ game/src/jegmezo/GameController.java
@@ -47,10 +47,26 @@ public class GameController {
 			System.out.println("start game command ran");
 			players.clear();
 			tiles.clear();
-			// TODO: Set players up from input
-			players.add(new Eskimo(this, 1));
-			players.add(new Scientist(this, 2));
-			players.add(new Eskimo(this, 3));
+			System.out.println("Number of players (3-8): ");
+			int number_of_player;
+			do{
+				number_of_player = scanner.nextInt();
+				if(number_of_player < 3 || number_of_player > 8)
+					System.out.println("Invalid value, try again!");
+			} while(number_of_player < 3 || number_of_player > 8);
+			for(int i = 0; i< number_of_player; i++){
+				System.out.println("Player "+ i + " class (eskimo/scientist): ");
+				String type = scanner.nextLine();
+				type.toLowerCase();
+				if(type.equals("eskimo")){
+					players.add(new Eskimo(this, i));
+				} else if (type.equals("scientist")){
+					players.add(new Scientist(this, i));
+				} else {
+					System.out.println("Wrong class, try again!");
+					i--;
+				}
+			}
 			LevelGenerator generator = new LevelGenerator(this, players.size());
 
 			for (Tile tile: generator.generate()) {
@@ -116,7 +132,19 @@ public class GameController {
 			player.takeTurn();
 			if (gameState != GameState.Running) return;
 		}
-		blizzard();
+		if(isControlledRandomness()){
+			Random r = new Random();
+			int blizzard_is_coming = r.nextInt(2);
+			if(blizzard_is_coming == 1){
+				blizzard();
+			}
+		} else {
+			String isBlizzard = scanner.nextLine();
+			isBlizzard.toLowerCase();
+			if(isBlizzard.equals("blizzard"))
+				blizzard();
+		}
+
 		polarBear.move();
 
 		//a kör végén minden sátor törlődik, ami fel volt építve

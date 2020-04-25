04a508db9b7a31981c3ef948da48f9bae9695318
MexPain <61620062+MexPain@users.noreply.github.com>
2020-04-25 11:01:41 +0200

    maci

--- game/src/jegmezo/PolarBear.java
+++ game/src/jegmezo/PolarBear.java
@@ -1,5 +1,9 @@
 package jegmezo;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class PolarBear extends Entity{
 
     /**
@@ -12,14 +16,52 @@ public class PolarBear extends Entity{
         tile.addPolarBear(this);
     }
 
+    /**
+     * A medve átlép egy szpmszédos jégtáblára.
+     * @return Sikerült e átlépni
+     */
     @Override
     public boolean move() {
-        return false;
+        ArrayList<Tile> neighbourTiles= new ArrayList<Tile>();
+        neighbourTiles.addAll(this.tile.getNeighbours());
+        Random rand = new Random();
+        int random_mezo = rand.nextInt(3);
+
+        boolean moveDone= false;
+        while( !moveDone || !neighbourTiles.isEmpty() ){
+            if (neighbourTiles.get(random_mezo).getPlayerLimit() ==0){
+                neighbourTiles.remove(random_mezo);
+            }else{
+                System.out.println("Polarbear moved to" + neighbourTiles.get(random_mezo).toShortString() );
+                neighbourTiles.get(random_mezo).stepOnPolarBear(this, tile);
+            }
+        }
+
+            System.out.println("Polarbear can't move");
+            return false;
     }
 
     public Tile selectTile() {
 
-        return tile.getNeighbours().get(0);
+        System.out.println("Neighbouring tiles: ");
+        ArrayList<Tile> neighbourTiles= new ArrayList<Tile>();
+        neighbourTiles.addAll(this.tile.getNeighbours());
+        for (Tile tile: neighbourTiles){
+            tile.toLongString();
+        }
+
+        while (true){
+            System.out.println("Select tile (<ID>/cancel)");
+            String line = new Scanner(System.in).nextLine().trim();
+
+            if(line.equals("cancel")) return null;
+            for (Tile tile : neighbourTiles) {
+                if (String.valueOf(tile.getId()).equals(line)) {
+                    return tile;
+                }
+            }
+            System.out.println("No tile with " + line + " ID.");
+        }
     }
 
     public void serialize() {

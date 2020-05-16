4da4840dc747c73b28256d817d60616fd4745910
bea <koltaib.email@gmail.com>
2020-05-16 20:13:38 +0200

    blizzard view

--- game/src/jegmezo/view/BlizzardOverlayView.java
+++ game/src/jegmezo/view/BlizzardOverlayView.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 //ezt az osztályt még tanácsos lenne szétszedni - Blizzard Overlay és GameOver Overlay
 public class BlizzardOverlayView extends View {
-    private String type;
+    private String type="Jön a HAV!";
     public BlizzardOverlayView(GameWindow gameWindow, AssetManager assetManager, String type) {
         super(gameWindow, assetManager);
         this.type = type;
@@ -14,6 +14,10 @@ public class BlizzardOverlayView extends View {
     public void draw(Graphics2D graphics, boolean overlay) {
         graphics.drawImage(assetManager.getImage("blizzardOverlay1"), 0, 0, GameWindow.windowWidth, GameWindow.windowHeight, null);
         graphics.drawImage(assetManager.getImage("blizzardOverlay2"), 0, 0, GameWindow.windowWidth, GameWindow.windowHeight, null);
+        Font font = new Font("Serif", Font.BOLD, 70);
+        graphics.setColor(Color.BLUE);
+        graphics.setFont(font);
+        graphics.drawString(type, GameWindow.windowWidth/2-200, GameWindow.windowHeight/2-5);
     }
 
     @Override

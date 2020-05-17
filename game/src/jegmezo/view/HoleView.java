19e0978fbdcd7a0c1a00ebe6ae8a54cabf7a5f3e
MexPain <61620062+MexPain@users.noreply.github.com>
2020-05-17 17:56:42 +0200

    holeview comment

--- game/src/jegmezo/view/HoleView.java
+++ game/src/jegmezo/view/HoleView.java
@@ -5,17 +5,37 @@ import jegmezo.model.Tile;
 import java.awt.*;
 import java.awt.event.HierarchyBoundsAdapter;
 
+/**
+ * Egy hole megjelenítéséért felel.
+ */
 public class HoleView extends TileView {
-
+    /**
+     * Konstruktor. Meghívja az ős konstruktorát.
+     * @param gameWindow a játék gamewindow-ja
+     * @param assetManager a játék assetManager-e
+     * @param levelView a levelView-ja a játéknak
+     * @param x a hole x koordinátája
+     * @param y a hole y koordinátája
+     * @param tile a tile, amit a HoleView megjelenít.
+     */
     public HoleView(GameWindow gameWindow, AssetManager assetManager, LevelView levelView, int x, int y, Tile tile) {
         super(gameWindow, assetManager, levelView, x, y, tile);
     }
 
+    /**
+     *  Megmondja hogy a HoleView-t befolyásolja e a transformáció.
+     * @return
+     */
     @Override
     public boolean isAffectedByTransformation() {
         return true;
     }
 
+    /**
+     * Kirajzolja a hole-t
+     * @param graphics a grafikát megvalósítő osztály
+     * @param overlay ha átfedésben van, akkor visszatér, ha nincs, akkor rajzol ki bármit
+     */
     @Override
     public void draw(Graphics2D graphics, boolean overlay) {
         if (!overlay) {

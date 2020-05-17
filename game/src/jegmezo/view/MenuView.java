4db2af1567d473ab55de3009781c2e28c1a27024
Péter Attila Rigó <rigopeter96@gmail.com>
2020-05-17 15:00:00 +0200

    Menuview commenttel ellátva

--- game/src/jegmezo/view/MenuView.java
+++ game/src/jegmezo/view/MenuView.java
@@ -6,13 +6,28 @@ import java.util.List;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 
+/**
+ * Kiírja a játékos eseményeit egy menülistában
+ */
 public class MenuView extends View {
+    /**
+     * A kirajzolás koordinátái
+     */
     private int x, y;
 
+    /**
+     * Konstruktor
+     * @param gameWindow játékablak
+     * @param assetManager képkirajzoló
+     */
     public MenuView(GameWindow gameWindow, AssetManager assetManager) {
         super(gameWindow, assetManager);
     }
 
+    /**
+     * beállítja a kiírandó menüelemeket
+     * @param items a kiírandó menüelemek
+     */
     public void setActionList(List<NamedAction> items) {
         int y = 0;
         children.clear();
@@ -25,30 +40,57 @@ public class MenuView extends View {
         }
     }
 
+    /**
+     * beálítja az x értéket
+     * @param x a beállítandó érték
+     */
     public void setX(int x) {
         this.x = x - 100;
     }
 
+    /**
+     * beálítja az y értéket
+     * @param y a beállítandó érték
+     */
     public void setY(int y){
         this.y = y - children.size() * 30;
     }
 
+    /**
+     * klikk event hatására meghívódik a függvény
+     * @param event az egéresemény
+     */
     @Override
     public void windowClicked(MouseEvent event) {
         super.windowClicked(event);
         if (!hovered) this.gameWindow.closeMenu();
     }
 
+    /**
+     * A kirajzolást végzá függvény
+     * @param graphics a grafikát megvalósítő osztály
+     * @param overlay ha átfedésben van, akkor visszatér, ha nincs, akkor rajzol ki bármit
+     */
     @Override
     public void draw(Graphics2D graphics, boolean overlay) {
         super.draw(graphics, overlay);
     }
 
+    /**
+     * A klikkeléskor kirajzolást valósítja meg
+     * @param x x koordináta
+     * @param y y koordináta
+     * @return visszatér a kirajzolással (true esetén kirajzolt valamit, egyébként false)
+     */
     @Override
     public boolean isMouseOver(int x, int y) {
         return new Rectangle(this.x, this.y, 100, children.size() * 30).contains(x, y);
     }
 
+    /**
+     * Történt-e transzformáció - mindig false
+     * @return mindig false
+     */
     @Override
     public boolean isAffectedByTransformation() {
         return false;

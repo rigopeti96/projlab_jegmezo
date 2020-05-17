79561fbbc71219dcc4928e22cc3b3e6c3357d677
bea <koltaib.email@gmail.com>
2020-05-17 17:05:58 +0200

    assetManager komment

--- game/src/jegmezo/view/AssetManager.java
+++ game/src/jegmezo/view/AssetManager.java
@@ -10,11 +10,26 @@ import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * AssetManager osztály, képek, színek, betűtípusok és egyéb kirajzoláshoz szükséges dolgokat valósít meg
+ */
 public class AssetManager {
+    /**
+     * a használatban levő betűtípus
+     */
     Map<String, Font> fontMap = new HashMap<>();
+    /**
+     * a tárolt színeket tartalmazó map
+     */
     Map<String, Color> colorMap = new HashMap<>();
+    /**
+     * a tárolt képeket tartalmazó map
+     */
     Map<String, BufferedImage> imageMap = new HashMap<>();
 
+    /**
+     * betölti a használni kívánt asset-eket, betűtípus, stb.
+     */
     public void loadAssets() {
         fontMap.put("Default", new Font("Calibri", Font.BOLD, 14));
         fontMap.put("Blizzard", new Font("Serif", Font.BOLD, 70));
@@ -39,6 +54,11 @@ public class AssetManager {
         loadImage("fog", "images/Fog-icon.png");
     }
 
+    /**
+     * egy képet tölt be
+     * @param name - a tárolt kép neve
+     * @param fileName - a kép elérési neve
+     */
     private void loadImage(String name, String fileName) {
         try {
             imageMap.put(name, ImageIO.read(Path.of(fileName).toFile()));
@@ -48,34 +68,74 @@ public class AssetManager {
         }
     }
 
+    /**
+     * visszaadja az alap használt betűtípust
+     * @return - a betűtípus
+     */
     public Font getFont() {
         return getFont("Default");
     }
 
+    /**
+     * visszaadja az alap betűtípust, megadott betűmérettel
+     * @param size - a betűméret
+     * @return - a betűtípus
+     */
     public Font getFont(float size) {
         return getFont("Default", size);
     }
 
+    /**
+     * visszaadja a kért betűtípust név szerint
+     * @param name - a betűtípus neve
+     * @return - a kért betűtípus
+     */
     public Font getFont(String name) {
         return fontMap.containsKey(name) ? fontMap.get(name) : fontMap.get("Default");
     }
 
+    /**
+     * visszaadja a kért betűtípust a kért betűmérettel
+     * @param name - a betűtípus neve
+     * @param size - a betűméret
+     * @return - a kért betűtípus
+     */
     public Font getFont(String name, float size) {
         return fontMap.containsKey(name) ? fontMap.get(name).deriveFont(size) : fontMap.get("Default").deriveFont(size);
     }
 
+    /**
+     * visszaadja a kért színt, név szerint
+     * @param name - a szín neve
+     * @return - a kért szín
+     */
     public Color getColor(String name) {
         return colorMap.getOrDefault(name, Color.black);
     }
 
+    /**
+     * visszaadja a képet, név szerint
+     * @param name - a kép neve
+     * @return - a kép
+     */
     public BufferedImage getImage(String name) {
         return imageMap.containsKey(name) ? imageMap.get(name) : imageMap.get("missingTexture");
     }
 
+    /**
+     * visszaadja a képet, név szerint, fekete-fehér színekkel
+     * @param name - a kép neve
+     * @return - a fekete-fehér kép
+     */
     public BufferedImage getImageGrayScale(String name) {
         return imageMap.containsKey(name) ? imageMap.get(name + "-grayscale") : imageMap.get("missingTexture-grayscale");
     }
 
+    /**
+     * egy képet fekete-fehérré alakít
+     * @param master - az eredeti kép
+     * @return - a fekete-fehér kép
+     */
     private BufferedImage toGrayScale(BufferedImage master) {
         if (master == null) master = this.getImage("missingTexture");
         BufferedImage gray = new BufferedImage(master.getWidth(), master.getHeight(), BufferedImage.TYPE_INT_ARGB);
